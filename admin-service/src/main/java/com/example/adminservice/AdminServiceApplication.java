46fb437c26a98fc458ecff304b271afe12fc9478
hyosunghan <ihyosung@qq.com>
2019-07-27 19:16:00 +0800

    fix

--- admin-service/src/main/java/com/example/adminservice/AdminServiceApplication.java
+++ admin-service/src/main/java/com/example/adminservice/AdminServiceApplication.java
@@ -3,25 +3,21 @@ package com.example.adminservice;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableEurekaClient
 @EnableAdminServer
-@EnableTurbine
-@EnableHystrixDashboard
-@EnableHystrix
+//@EnableTurbine
+//@EnableHystrixDashboard
+//@EnableHystrix
 @SpringBootApplication
 public class AdminServiceApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AdminServiceApplication.class);
-    }
+//      war包需要
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(AdminServiceApplication.class);
+//    }
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
     }
--- admin-service/src/main/java/com/example/adminservice/security/SecurityConfig.java
+++ admin-service/src/main/java/com/example/adminservice/security/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.adminservice.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author hyosunghan
@@ -10,16 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @date 2019-07-23 23:18
  */
 @Configuration
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-        http.logout().logoutUrl("/logout");
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/login.html", "/**/**.css", "/img/**", "/third-party/**").permitAll();
-        http.authorizeRequests().antMatchers("/**").authenticated();
-        http.httpBasic();
-    }
+//    private final String adminContextPath;
+//
+//    public SecurityConfig(AdminServerProperties adminServerProperties) {
+//        this.adminContextPath = adminServerProperties.getContextPath();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+//        http.logout().logoutUrl("/logout");
+//        http.csrf().disable();
+//        http.authorizeRequests().antMatchers("/login.html", "/**/**.css", "/img/**", "/third-party/**").permitAll();
+//        http.authorizeRequests().antMatchers("/**").authenticated();
+//        http.httpBasic();
+//    }
 }
