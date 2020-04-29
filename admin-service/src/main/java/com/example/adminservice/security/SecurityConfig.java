1345a005538501ff05177878114f37d3208a8dae
hyosunghan <ihyosung@qq.com>
2020-04-29 11:54:56 +0800

    fix

--- admin-service/src/main/java/com/example/adminservice/security/SecurityConfig.java
+++ admin-service/src/main/java/com/example/adminservice/security/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.example.adminservice.security;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
  * @author hyosunghan
@@ -8,22 +12,34 @@ import org.springframework.context.annotation.Configuration;
  * @date 2019-07-23 23:18
  */
 @Configuration
-class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    private final String adminContextPath;
-//
-//    public SecurityConfig(AdminServerProperties adminServerProperties) {
-//        this.adminContextPath = adminServerProperties.getContextPath();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
+    private final String adminContextPath;
+
+    public SecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
 //        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
 //        http.logout().logoutUrl("/logout");
 //        http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/login.html", "/**/**.css", "/img/**", "/third-party/**").permitAll();
 //        http.authorizeRequests().antMatchers("/**").authenticated();
 //        http.httpBasic();
-//    }
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter( "redirectTo" );
+
+        http.authorizeRequests()
+                .antMatchers( adminContextPath + "/assets/**" ).permitAll()
+                .antMatchers( adminContextPath + "/login" ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage( adminContextPath + "/login" ).successHandler( successHandler ).and()
+                .logout().logoutUrl( adminContextPath + "/logout" ).and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
 }
